25884a16fedcd95795e48c18f6cc6d21dc8d7adf
Austen <lock544@proton.me>
2025-08-02 17:12:20 -0500

    Added react router for easy page navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import "./App.css";
 import TodoApp from "./components/TodoApp/TodoApp";
 import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <>
       <Navbar />
-      <div className='app'>
-        <TodoApp />
-      </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </>
   );
 }
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoApp from "../components/TodoApp/TodoApp";
+
+export default function Home() {
+  return (
+    <div>
+      <div className='app'>
+        <TodoApp />
+      </div>
+    </div>
+  );
+}
