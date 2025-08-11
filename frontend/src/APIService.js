78e9182338798b5e2725967b94157e23ed8b83af
Austen <lock544@proton.me>
2025-08-11 18:04:23 -0500

    test

--- frontend/src/APIService.js
+++ frontend/src/APIService.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
 
-const API_BASE_URL = import.meta.env.production.VITE_API_BASE_URL;
+dotenv.config();
+
+const API_BASE_URL = process.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
